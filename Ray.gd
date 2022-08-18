2bf04c2276194c32c6f17a07d9363e41c4af8e54
aarond707 <aarond707@gmail.com>
2022-08-18 13:01:00 -0700

    minor update

--- Ray.gd
+++ Ray.gd
@@ -1,23 +1,14 @@
 extends Spatial
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var in_dialog = false
 
+func _on_Dialog_dialogic_signal(value):
+	if value == "start":
+		in_dialog = true
+	if value == "end":
+		in_dialog == false
 
 func _on_Player_interact(node):
-	if node == $RaySit01/RayStaticBody:
+	if node == $RaySit01/RayStaticBody and in_dialog == false:
 		var new_dialog = Dialogic.start('/timeline-1660331307')
 		add_child(new_dialog)
-
-#	if node == $ButtonHull/ButtonMesh/Area\
-#	and not $AnimationPlayer.is_playing():
-#		# if Instant is true, an animation will play to which the button will be pressed once.
-#		if Instant:
-#			$AnimationPlayer.play("press")
-#		# otherwise, an animation will be played the button will be held.
-#		else:
-#			$AnimationPlayer.play("regular press")
-#		# We then send a signal in case it is connected to a door.
-#		emit_signal("buttonPress",self)
-#		$PressSound.play()
