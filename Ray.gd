3f5635ce571889b02bdc8bd8d55e3e50264dc24c
aarond707 <aarond707@gmail.com>
2022-08-18 12:18:41 -0700

    NPC dialog activation
    the NPC dialog no longer requires the button being clicked. you can now just click or press e on the npc to activate dialog.

--- Ray.gd
+++ Ray.gd
@@ -4,6 +4,20 @@ extends Spatial
 func _ready():
 	pass # Replace with function body.
 
-func _on_Button_buttonPress(node):
-	var new_dialog = Dialogic.start('/timeline-1660331307')
-	add_child(new_dialog)
+
+func _on_Player_interact(node):
+	if node == $RaySit01/RayStaticBody:
+		var new_dialog = Dialogic.start('/timeline-1660331307')
+		add_child(new_dialog)
+
+#	if node == $ButtonHull/ButtonMesh/Area\
+#	and not $AnimationPlayer.is_playing():
+#		# if Instant is true, an animation will play to which the button will be pressed once.
+#		if Instant:
+#			$AnimationPlayer.play("press")
+#		# otherwise, an animation will be played the button will be held.
+#		else:
+#			$AnimationPlayer.play("regular press")
+#		# We then send a signal in case it is connected to a door.
+#		emit_signal("buttonPress",self)
+#		$PressSound.play()
