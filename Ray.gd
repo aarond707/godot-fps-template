6b981b5f114d780de8367d02c9729132fbd6557e
aarond707 <aarond707@gmail.com>
2022-08-19 14:08:58 -0700

    minor
    last update had sounds added also, forgot to note

--- Ray.gd
+++ Ray.gd
@@ -2,12 +2,6 @@ extends Spatial
 
 var in_dialog = false
 
-func _on_Dialog_dialogic_signal(value):
-	if value == "start":
-		in_dialog = true
-	if value == "end":
-		in_dialog == false
-
 func _on_Player_interact(node):
 	if node == $RaySit01/RayStaticBody and in_dialog == false:
 		var new_dialog = Dialogic.start('/timeline-1660331307')
