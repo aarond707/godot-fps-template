f4da2301a3b260fb4abd1aec48be8124d4307f2c
aarond707 <aarond707@gmail.com>
2022-08-22 17:23:01 -0700

    issues with signals
    see godot notes

--- Ray.gd
+++ Ray.gd
@@ -1,8 +1,20 @@
 extends Spatial
 
-var in_dialog = false
+onready var in_dialog = false
 
+func dialog_started(timeline_name):
+	if timeline_name == "ray_timeline":
+		in_dialog = true
+		print (in_dialog)
+
+func dialog_ended(timeline_name):
+	if timeline_name == "ray_timeline":
+		in_dialog = false
+		print (in_dialog)
+	
 func _on_Player_interact(node):
 	if node == $RaySit01/RayStaticBody and in_dialog == false:
-		var new_dialog = Dialogic.start('/timeline-1660331307')
-		add_child(new_dialog)
+		var dialog = Dialogic.start('/ray_timeline')
+		dialog.connect("timeline_start", self, "dialog_started")
+		dialog.connect("timeline_end", self, "dialog_ended")
+		add_child(dialog)
