589446147111dabd7af1a0360dc20c44c24b8cfe
jordanarytime <111407317+jordanarytime@users.noreply.github.com>
2022-08-26 16:09:54 -0700

    junkyard

--- gridlessDB_data/Database/classes/Characters.gd
+++ gridlessDB_data/Database/classes/Characters.gd
@@ -4,4 +4,4 @@ class_name Characters
 export (String) var name = ""
 export (String) var favorite_food = ""
 export (String) var goal = ""
-export (Resource) var closest_friend = preload("res://gridlessDB_data/Database/objects/Ray.tres")
+export (Resource) var original_home = preload("res://gridlessDB_data/Database/objects/TheCoil.tres")
