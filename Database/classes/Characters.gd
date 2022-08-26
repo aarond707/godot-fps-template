73f9ec70a246a9678b5eb2c9ac1ee9a053f54981
aarond707 <aarond707@gmail.com>
2022-08-26 15:35:25 -0700

    fix(?????)

--- Database/classes/Characters.gd
+++
@@ -1,7 +0,0 @@
-extends World
-class_name Characters
-
-export (String) var name = ""
-export (String) var portrait = "C:/Users/aaron/Documents/GitHub/thishorriblemachine/Portraits/ray00.png"
-export (String) var favorite_food = ""
-export (String) var goal = ""
--- Database/classes/Items.gd
+++
@@ -1,4 +0,0 @@
-extends World
-class_name Items
-
-export (String) var name = ""
