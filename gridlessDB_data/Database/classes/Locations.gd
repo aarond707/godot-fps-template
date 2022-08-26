a068d796bf7f7e170c0db239e6c7f20e8a436f74
jordanarytime <111407317+jordanarytime@users.noreply.github.com>
2022-08-26 16:41:15 -0700

    dialogicstuff

--- gridlessDB_data/Database/classes/Locations.gd
+++ gridlessDB_data/Database/classes/Locations.gd
@@ -4,4 +4,4 @@ class_name Locations
 export (bool) var breathable = 0
 enum DecayPresences {None, Low, High, Pure}
 export(DecayPresences) var decay_presence = DecayPresences.None
-export (bool) var interior/exterior = 0
+export (Resource) var shelter = preload("res://gridlessDB_data/Database/objects/Interior.tres")
